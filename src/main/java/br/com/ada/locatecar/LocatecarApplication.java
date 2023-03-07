e2c85d86badedcfe1b05ee6e03153dcee107a557
Pedro <pedrohenrique096k@gmail.com>
2023-03-07 17:11:33 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/ada/locatecar/LocatecarApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.locatecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocatecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocatecarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ada/locatecar/LocatecarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.locatecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocatecarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
