7159b476e7189d4d27d171d58b6b82060e29bea2
Pedro <pedrohenrique096k@gmail.com>
2023-03-10 18:35:45 -0300

    Implementando metodo para buscar cliente pelo id

--- src/main/java/br/com/ada/locatecar/service/ClientePJService.java
+++ src/main/java/br/com/ada/locatecar/service/ClientePJService.java
@@ -1,6 +1,7 @@
 package br.com.ada.locatecar.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,10 @@ public class ClientePJService {
 
     public void removerCliente(Long id){
         this.clientePJRepository.deleteById(id);
+    }   
+
+    public Optional<ClientePJ> buscarClientePorId(Long id){
+        return this.clientePJRepository.findById(id);
     }
 
 }
