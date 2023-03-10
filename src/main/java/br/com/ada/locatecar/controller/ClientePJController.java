434b545c10c49584ef25bca091962d00c549dfbb
Pedro <pedrohenrique096k@gmail.com>
2023-03-10 18:36:12 -0300

    Adaptando classe controller para o thymeleaf

--- src/main/java/br/com/ada/locatecar/controller/ClientePJController.java
+++ src/main/java/br/com/ada/locatecar/controller/ClientePJController.java
@@ -1,38 +1,64 @@
 package br.com.ada.locatecar.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ada.locatecar.model.ClientePJ;
 import br.com.ada.locatecar.service.ClientePJService;
 
-@RestController
-@RequestMapping("/clientepj")
+@Controller
 public class ClientePJController {
-    
+
     @Autowired
     private ClientePJService clientePJService;
 
-    @PostMapping("/add")
-    public void adicionarCliente(@RequestBody ClientePJ cliente){
+    @GetMapping("/clientepj")
+    public String clientespj(Model model) {
+        List<ClientePJ> clientes = this.clientePJService.listarTodosOsClientesPJ();
+        model.addAttribute("clientespj", clientes);
+        return "clientepj";
+    }
+
+    @GetMapping("/clientepj/add")
+    public String clienteAdd(Model model) {
+        model.addAttribute("clientepj", new ClientePJ());
+        return "clientepj-add";
+    }
+
+    @PostMapping("/clientepj/add")
+    public String adicionarCliente(@ModelAttribute("clientepj") ClientePJ cliente) {
         this.clientePJService.adicionarCliente(cliente);
+        return "redirect:/clientepj";
     }
 
-    @DeleteMapping("/remove/{id}")
-    public void removerCliente(@PathVariable Long id){
+    @GetMapping("/clientepj/{id}/delete")
+    public String removerCliente(@PathVariable("id") Long id) {
         this.clientePJService.removerCliente(id);
+        return "redirect:/clientepj";
     }
 
-    @GetMapping("/todos")
-    public List<ClientePJ> listarTodosOsClientesPJ(){
-        return this.clientePJService.listarTodosOsClientesPJ();
+    @GetMapping("/clientepj/{id}/edit")
+    public String mostrarEditarCliente(Model model, @PathVariable("id") Long id){
+        Optional<ClientePJ> optionalCliente = this.clientePJService.buscarClientePorId(id);
+
+        optionalCliente.ifPresent(cliente -> model.addAttribute("clientepj", cliente));
+
+        return "clientepj-edit";
     }
+
+    @PostMapping("/clientepj/{id}/edit")
+    public String editarCliente(@ModelAttribute("clientepj") ClientePJ cliente, @PathVariable("id") Long id) {
+        cliente.setId(id);
+        this.clientePJService.adicionarCliente(cliente);
+        return "redirect:/clientepj";
+    }
+
 }
