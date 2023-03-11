a43a389ed6e5e0f4b37bf9164ba73c49d5f02374
Pedro <pedrohenrique096k@gmail.com>
2023-03-11 01:19:04 -0300

    Feat: validando form de cadastro
    Implementando função para exibir erros de campo em branco do CNPJ. Validação e tratamento de erro em caso de tentativa de registro de um CNPJ já existente na base dados

--- src/main/java/br/com/ada/locatecar/controller/ClientePJController.java
+++ src/main/java/br/com/ada/locatecar/controller/ClientePJController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.ada.locatecar.model.ClientePJ;
 import br.com.ada.locatecar.service.ClientePJService;
+import jakarta.validation.Valid;
 
 @Controller
 public class ClientePJController {
@@ -34,9 +36,19 @@ public class ClientePJController {
     }
 
     @PostMapping("/clientepj/add")
-    public String adicionarCliente(@ModelAttribute("clientepj") ClientePJ cliente) {
-        this.clientePJService.adicionarCliente(cliente);
-        return "redirect:/clientepj";
+    public String adicionarCliente(@Valid @ModelAttribute("clientepj") ClientePJ cliente, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "clientepj-add";
+        }
+
+        try {
+            this.clientePJService.adicionarCliente(cliente);
+            return "redirect:/clientepj";
+        } catch (Exception e) {
+            model.addAttribute("erroCnpj", "Esse CNPJ já Existe!");
+            return "clientepj-add";
+        }
+
     }
 
     @GetMapping("/clientepj/{id}/delete")
@@ -46,7 +58,7 @@ public class ClientePJController {
     }
 
     @GetMapping("/clientepj/{id}/edit")
-    public String mostrarEditarCliente(Model model, @PathVariable("id") Long id){
+    public String mostrarEditarCliente(Model model, @PathVariable("id") Long id) {
         Optional<ClientePJ> optionalCliente = this.clientePJService.buscarClientePorId(id);
 
         optionalCliente.ifPresent(cliente -> model.addAttribute("clientepj", cliente));
@@ -59,6 +71,13 @@ public class ClientePJController {
         cliente.setId(id);
         this.clientePJService.adicionarCliente(cliente);
         return "redirect:/clientepj";
+    }
+
+    @GetMapping("/clientepj/{id}/view")
+    public String mostrarViewCliente(Model model, @PathVariable("id") Long id) {
+        Optional<ClientePJ> optionalCliente = this.clientePJService.buscarClientePorId(id);
+        optionalCliente.ifPresent(cliente -> model.addAttribute("clientepj", cliente));
+        return "clientepj-view";
     }
 
 }
